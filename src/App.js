ffd17fbbe6abdf001b6b0bda04e9bc721fe7f4bf
kyinn1307 <shruddls1307@gmail.com>
2024-07-23 06:44:05 +0900

    remove div whose class name is App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 // src/App.js
 import React from "react";
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import TeachableMachine from './components/TeachableMachine';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import TeachableMachine from "./pages/TeachableMachine";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-            <header className="App-header">
-             <h1>My App</h1>
-             </header>
-             <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/teachable-machine" element={<TeachableMachine />} />
-        </Routes>
-      </div>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/teachable-machine" element={<TeachableMachine />} />
+      </Routes>
     </Router>
-    );
+  );
 }
 
 export default App;
