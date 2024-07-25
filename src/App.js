1beffc7b4173b6ff87c83035ad275ceaba9555b3
kyinn1307 <shruddls1307@gmail.com>
2024-07-25 17:37:16 +0900

    modify the path

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
-import TeachableMachine from "./pages/TeachableMachine";
+import Login from "./pages/LoginPage";
+import TeachableMachine from "./pages/TeachableMachinePage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/teachable-machine" element={<TeachableMachine />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/teachable-machine" element={<TeachableMachinePage />} />
       </Routes>
     </Router>
   );
