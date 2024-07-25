bb35989a64336a66c1683d7b780fa13daca8ce96
kyinn1307 <shruddls1307@gmail.com>
2024-07-25 18:12:46 +0900

    create feedback page

---
+++ src/pages/FeedBackPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Record from "../components/Record";
+
+function FeedBackPage() {
+  return (
+    <div className="feedBack-container">
+      <h2>FeedBack</h2>
+      <Record />
+    </div>
+  );
+}
+
+export default FeedBackPage;
