6054bbd167852a9f5c4f042e70f702e7879e935c
kyinn1307 <shruddls1307@gmail.com>
2024-07-25 18:13:26 +0900

    create mainpage

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function MainPage() {
+  const navigate = useNavigate();
   return (
     <div className="main-container">
       <h2>Main</h2>
+      <button
+        onClick={() => {
+          navigate("/login");
+        }}
+      >
+        login
+      </button>
+      <button
+        onClick={() => {
+          navigate("/feedback");
+        }}
+      >
+        feed back
+      </button>
+      <button
+        onClick={() => {
+          navigate("/teachable-machine");
+        }}
+      >
+        scan
+      </button>
     </div>
   );
 }
