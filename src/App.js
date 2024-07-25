c06c023b74ba869f458024d373a5c9f4e5ea451a
kyinn1307 <shruddls1307@gmail.com>
2024-07-25 18:13:48 +0900

    create feedback page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./pages/LoginPage";
-import TeachableMachine from "./pages/TeachableMachinePage";
+import LoginPage from "./pages/LoginPage";
+import TeachableMachinePage from "./pages/TeachableMachinePage";
+import MainPage from "./pages/MainPage";
+import FeedBackPage from "./pages/FeedBackPage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/teachable-machine" element={<TeachableMachinePage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/feedback" element={<FeedBackPage />} />
+      <Route path="/teachable-machine" element={<TeachableMachinePage />} />
+    </Routes>
   );
 }
 
