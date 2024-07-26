5b0b9e2933098ac828981a23fde9abb1703d0128
kyinn1307 <shruddls1307@gmail.com>
2024-07-26 16:46:59 +0900

    add text area for feedback

--- src/components/Record.js
+++ src/components/Record.js
@@ -1,15 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 
 const Record = () => {
+  const [store, setStore] = useState("");
+
   const {
     transcript,
     listening,
     resetTranscript,
     browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
+
+  useEffect(() => {
+    let copy = transcript;
+    setStore(copy);
+  }, [transcript]);
+
+  const handleRecordOn = () => {
+    SpeechRecognition.startListening({ continuous: true });
+  };
+
+  const handleRecordOff = () => {
+    SpeechRecognition.stopListening();
+  };
+
+  const handleChange = (event) => {
+    const text = event.target.value;
+    if (text.length <= 255) {
+      setStore(text);
+    }
+  };
+
+  const handleReset = () => {
+    handleRecordOff();
+    resetTranscript();
+    setStore("");
+  };
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
@@ -18,10 +46,17 @@ const Record = () => {
   return (
     <div>
       <p>Microphone: {listening ? "on" : "off"}</p>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
+      <button onClick={handleRecordOn}>Start</button>
+      <button onClick={handleRecordOff}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
       <p>{transcript}</p>
+      <textarea
+        name="store"
+        placeholder="음성 혹은 타이핑을 통해 문구를 입력하세요"
+        value={store}
+        onChange={handleChange}
+        maxLength={255}
+      ></textarea>
     </div>
   );
 };
