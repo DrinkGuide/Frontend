c8bc4dd0f369c51f036cbd88ad2cee19aa86ff18
kyinn1307 <shruddls1307@gmail.com>
2024-07-29 05:05:24 +0900

    add Cart component

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -1,17 +1,36 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 function MyPage() {
+  const navigate = useNavigate();
   return (
     <div className="myPage-container">
       <h2>My Page</h2>
-      <button className="subscribe-btn">
-        <Link to="/mypage/subscribe">구독 현황</Link>
+      <button
+        className="subscribe-btn"
+        onClick={() => {
+          navigate("/mypage/subscribe");
+        }}
+      >
+        구독 현황
       </button>
-      <button className="history-btn">
-        <Link to="/mypage/history">구매 기록</Link>
+      <button
+        className="history-btn"
+        onClick={() => {
+          navigate("/mypage/history");
+        }}
+      >
+        구매 기록
       </button>
-      <Outlet/>
+      <button
+        className="history-btn"
+        onClick={() => {
+          navigate("/mypage/cart");
+        }}
+      >
+        장바구니
+      </button>
+      <Outlet />
     </div>
   );
 }
--- src/pages/MyPage/components/History.js
+++ src/pages/MyPage/components/History.js
@@ -1,9 +1,9 @@
 import React from "react";
-
 function History() {
   return (
     <div className="subscribe-container">
       <h2>History</h2>
+      <button className="영양정보_음성">영양정보 조회</button>
     </div>
   );
 }
