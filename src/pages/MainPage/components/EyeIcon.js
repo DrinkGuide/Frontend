84c4ec9e8515d87f31104ffedafb4235b3e55523
kyinn1307 <shruddls1307@gmail.com>
2024-07-31 21:38:04 +0900

    Update Mainpage

---
+++ src/pages/MainPage/components/EyeIcon.js
@@ -0,0 +1,72 @@
+export const EyeIcon = () => {
+  return (
+    <svg
+      width="109"
+      height="104"
+      viewBox="0 0 109 104"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M54.3158 24.4077C35.3814 24.4077 16.447 33.5415 2 51.809C16.447 70.0765 35.3814 79.2102 54.3158 79.2102C73.2502 79.2102 92.1846 70.0765 106.632 51.809C92.1846 33.5415 73.2502 24.4077 54.3158 24.4077Z"
+        stroke="#FFFA87"
+        stroke-width="4"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M54.3158 77.9013C68.7582 77.9013 80.4661 66.2193 80.4661 51.8088C80.4661 37.3983 68.7582 25.7163 54.3158 25.7163C39.8733 25.7163 28.1654 37.3983 28.1654 51.8088C28.1654 66.2193 39.8733 77.9013 54.3158 77.9013Z"
+        stroke="#FFFA87"
+        stroke-width="4"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M54.3158 65.0301C61.6338 65.0301 67.5663 59.1108 67.5663 51.8089C67.5663 44.507 61.6338 38.5876 54.3158 38.5876C46.9977 38.5876 41.0652 44.507 41.0652 51.8089C41.0652 59.1108 46.9977 65.0301 54.3158 65.0301Z"
+        stroke="#FFFA87"
+        stroke-width="4"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M54.3157 14.375V2"
+        stroke="#FFFA87"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M75.4036 17.1614L79.9273 5.63892"
+        stroke="#FFFA87"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M33.2265 17.1614L28.704 5.63892"
+        stroke="#FFFA87"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M54.6327 89.6248V102"
+        stroke="#FFFA87"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M33.5435 86.8386L29.021 98.3611"
+        stroke="#FFFA87"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M75.7218 86.8386L80.2443 98.3611"
+        stroke="#FFFA87"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
