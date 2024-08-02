303fed2d9113c020005f10f28e4fb43a1cd69602
JiinHong <com5942@seoultech.ac.kr>
2024-08-02 14:39:35 +0900

    마이페이지 & 로그인 화면 수정

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Text } from "../../components/Text";
 import "./Login.css";
 import { Marquee } from "../../components/Marquee";
 import styled from "styled-components";
@@ -7,19 +8,31 @@ import { VoiceLabelText } from "../../components/VoiceLableText";
 import { ReactComponent as GoogleLogin } from "../../assets/images/google-login.svg";
 import { ReactComponent as GoogleSignup } from "../../assets/images/google-signup.svg";
 import { Footer } from "../../components/Footer";
+import { ScrollButton } from "../MainPage/components/ScrollButton";
+import { EyeIcon } from "../MainPage/components/EyeIcon";
+import { SpeechIcon } from "../MainPage/components/SpeechIcon";
+import { ReactComponent as Changing_icon_1 } from "../../assets/images/changing_icon_1.svg";
+
+const handleScroll = () => {
+  window.scrollTo({
+    top: 745,
+    behavior: "smooth",
+  });
+};
 
 const LoginContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: 655px;
-  width: 393px;
-  margin: 0 auto;
-  background-color: black;
+font-family: "Pretendard-Regular";
+display: flex;
+flex-direction: column;
+align-items: center;
+height: 2600px;
+width: 393px;
+margin: 0 auto;
+background-color: black;
 `;
 
 const TextContainer = styled.div`
-  padding-top: 1.03rem;
+  margin-bottom: 40px;
   color: #ffffff;
   text-align: center;
   font-family: "PretendardVariable-Bold", sans-serif;
@@ -32,16 +45,16 @@ const TextContainer = styled.div`
 `;
 
 const StyledGoogleLogin = styled(GoogleLogin)`
+    
   width: 280px;
   height: 100px;
   cursor: pointer;
 `;
 
-const StyledGoogleSignup = styled(GoogleSignup)`
-  width: 280px;
-  height: 100px;
-  margin-top: 1rem; /* Optional: adds space between buttons */
-`;
+const StyledChanging_icon_1 = styled(Changing_icon_1)`
+  width: auto;
+  height: 150px;
+`
 
 function LoginPage() {
   const handleGoogleLogin = () => {
@@ -56,16 +69,87 @@ function LoginPage() {
         <TextContainer>
           언제 어디서나 손쉽게 음성으로 만나는 정확한 정보
         </TextContainer>
-        <Marquee />
+        <Marquee paddingBottom="40px" />
+        <Text color="#FFFFFF" fontSize="23px" marginTop="60px" fontWeight="700">
+          별도의 번거로운 회원가입 절차 없이 <br />
+          구글 계정으로 만나보세요!
+        </Text>
         <StyledGoogleLogin
           onClick={() => {
             handleGoogleLogin();
           }}
         />
-        <StyledGoogleSignup />
-        <Marquee />
+        <Text
+          color="#FFFFFF"
+          fontSize="16px"
+          marginTop="60px"
+          paddingBottom="25px"
+        >
+          보이스라벨은 어떤 서비스인가요?
+        </Text>
+        <ScrollButton onClick={handleScroll} />
+        <Marquee paddingTop="60px" paddingBottom="100px" />
+        <span>
+          <EyeIcon />
+          <SpeechIcon />
+        </span>
+
+        <div className="group-12" id="scrollTarget">
+          <div className="rectangle-33"></div>
+          <div className="rectangle-34"></div>
+          <div className="rectangle-35"></div>
+          <div className="div">
+            <span>
+              <span className="div-span">정확</span>
+              <span className="div-span2">하고 </span>
+              <span className="div-span3">또박또박</span>
+              <span className="div-span4">
+                하게
+                <br />
+                그리고
+              </span>
+              <span className="div-span2"> </span>
+              <span className="div-span5">손쉽게</span>
+              <span className="div-span6">!</span>
+            </span>
+          </div>
+        </div>
+
+        <Text color="#FFFFFF" fontSize="14px" paddingTop="45px">
+          글자가 너무 작아서 잘 보이지 않는 크기로 적혀있거나,
+          <br />
+          빼곡하게 적혀 있어 눈에 잘 들어오지 않는 상품 정보를
+          <br />
+          정확하고 또박또박한 음성으로 손쉽게 알려드립니다.
+        </Text>
+        <Marquee paddingTop="145px" paddingBottom="137px" />
+
+        <StyledChanging_icon_1 />
+
+        <div class="group-12">
+          <span>
+            <span class="div-span">바로바로</span>
+            <span class="div-span2">
+              식별 가능한
+              <br />
+            </span>
+            <span class="div-span3">다양한 종류</span>
+            <span class="div-span4">의 </span>
+            <span class="div-span5">상품</span>
+            <span class="div-span6">들</span>
+          </span>
+        </div>
+
+        <Text color="#FFFFFF" fontSize="14px">
+          과자와 음료수부터 가공식품, 과일, 채소, 생선류 등!
+          <br />
+          다양한 상품의 정보를 빠르게 알려드립니다.
+        </Text>
+
+        <Marquee paddingTop="137px" paddingBottom="68px" />
+        <Footer />
       </LoginContainer>
-      <Footer />
+
     </>
   );
 }
--- src/pages/MyPage/components/HistoryPage.js
+++ src/pages/MyPage/components/HistoryPage.js
@@ -10,10 +10,10 @@ import styled from "styled-components";
 /*
 해야할 것들
 
-1. 아이콘 정리해서 구매 인증에 따라 아이콘 보이게 하는 로직 작성
+1. 아이콘 정리해서 구매 인증에 따라 아이콘 보이게 하는 로직 작성(백엔드랑 같이)
 2. 상품 버튼 누르면 상세정보 나오게 하는 애니메이션 구현
 3. footer에서 글자를 어떻게 좀 더 왼쪽으로 옮길지 고민..
-4. 마이페이지에서 링크 매핑하기
+4. 마이페이지에서 링크 매핑하기 - 완료
 5. 중복되는 것들 함수로 구현해서 정리하기
 */
 
@@ -83,11 +83,6 @@ const Circle = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-const PurchaseImage = styled.img`
-  width: 30px;
-  height: 30px;
 `;
 
 const HistoryPage = () => {
