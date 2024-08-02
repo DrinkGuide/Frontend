4275bfea4290cf78eb2f3b39ad38ee1d7307cc50
JuseungL <121665437+JuseungL@users.noreply.github.com>
2024-08-02 10:16:15 +0900

    BUG : /login충돌로 /signin으수정

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signin" element={<LoginPage />} />
         <Route path="/feedback" element={<FeedBackPage />} />
         <Route path="/teachable-machine" element={<TeachableMachinePage />} />
         <Route path="/mypage" element={<MyPage />} />
