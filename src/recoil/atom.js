f30241237ce4bb5e57c946c1e2c88de8e4ba049e
kyinn1307 <shruddls1307@gmail.com>
2024-08-03 00:09:37 +0900

    add atom about product type color

--- src/recoil/atom.js
+++ src/recoil/atom.js
@@ -4,3 +4,15 @@ export const SubscribeTypeAtom = atom({
   key: "SubscribeType",
   default: "",
 });
+
+export const ProductTypeColorAtom = atom({
+  key: "ProductTypeColor",
+  default: [
+    { DRINK: "#5D9EFF" },
+    { SNACK: "#FF5858" },
+    { RETORTFOOD: "#FFA858" },
+    { FRUIT: "#FFFA87" },
+    { VEGETABLE: "#85FF8A" },
+    { FISH: "#FF89C1" },
+  ],
+});
