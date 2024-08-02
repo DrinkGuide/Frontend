501e3c87239d5ef91faa1a3ab1a93df411bc749c
JiinHong <com5942@seoultech.ac.kr>
2024-08-02 13:15:46 +0900

    Main branch changes before switching to jinhong

--- src/pages/MyPage/components/HistoryPage.js
+++ src/pages/MyPage/components/HistoryPage.js
@@ -125,7 +125,7 @@ const HistoryPage = () => {
           <Circle></Circle>
         </PurchaseImageContainer>
         <HistoryTextBox fontSize="16px" fontColor="#FFFA87" margin="0 0 40px 0">
-          이번 달에는 구매 인증을 2회 했어요.
+          이번 달에는 구매 인증을 6회 했어요.
           <br />
           8회 더 인증 시 구독료 1,000원 할인 혜택이 있어요.
         </HistoryTextBox>
